5a7bde3a61e6a5d762c0ba0998519b920c1dce15
Victor Muñoz <victor.munoz@alumnos.ucn.cl>
2023-06-01 22:10:50 -0400

    PresentacionViernes

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, SafeAreaView, Image, Alert } from 'react-native';
+import Data from './src/Data';
+import { useState } from 'react';
 
 export default function App() {
+  // La función renderItem define cómo se renderiza cada ítem en el FlatList
+  const renderItem = ({ item }) => {
+    const alert = () => {
+      // Al presionar el TouchableOpacity, se muestra una alerta con el nombre y el precio del auto seleccionado
+      Alert.alert(item.name, "Precio: " + item.price, "OK");
+    };
+  
+    return (
+      <TouchableOpacity style={styles.button} onPress={alert}>
+        <Image source={item.image} style={styles.image} />
+        <Text style={styles.text}>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        {/* FlatList renderiza una lista de ítems */}
+        <FlatList
+          data={Data} // Los datos se obtienen del archivo Data.js importado
+          renderItem={renderItem} // La función renderItem se utiliza para renderizar cada ítem
+          keyExtractor={(item) => item.id} // Se utiliza el atributo "id" de cada ítem como clave
+          //horizontal={true} // Se renderiza la lista de forma horizontal
+          
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +41,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#f4f4f4',
+    borderRadius: 10,
+    paddingBottom: 40,
+    width: 400,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 70,
+  },
+  image: {
+    width: 400,
+    height: 200,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
---
+++ src/Data.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import GTR from '../assets/cars/gtr.jpg';
+import TURBOS from '../assets/cars/turbos.jpg';
+import WRXSTI from '../assets/cars/wrxsti.jpg';
+
+const Data = [
+    {
+        id: 1,
+        name: 'Nissan GTR',
+        price: "100.000.000",
+        image: GTR,
+    },
+    {
+        id: 2,
+        name: 'Porsche 911 Turbo S',
+        price: "200.000.000",
+        image: TURBOS,
+    },
+    {
+        id: 3,
+        name: 'Subaru WRX STI',
+        price: "28.000.000",
+        image: WRXSTI,
+    },
+        
+];
+export default Data
